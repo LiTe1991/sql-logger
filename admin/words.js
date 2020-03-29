8a32aa2aaf26cd4689ecde48f98ac24174fcb834
LiTe1991 <vadim19917@gmail.com>
2020-03-29 13:46:00 +0200

    Initial commit (create project)

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'sql-logger adapter settings': {
+        'en': 'Adapter settings for sql-logger',
+        'de': 'Adaptereinstellungen für sql-logger',
+    },
+    'sqlServerSettings_info': {
+        'en': 'Enter your sql server setting (necessary)',
+        'de': 'Tragen Sie ihre SQL-Server Einstellungen ein (notwendig)',
+    },
+    'sqlServerSettings': {
+        'en': 'Sql server settings',
+        'de': 'Einstellung SQL-Server',
+    },
+    'sqlServerSetting_hostname_label': {
+        'en': 'Hostname',
+        'de': 'Hostname',
+    },
+    'sqlServerSetting_port_label': {
+        'en': 'Port',
+        'de': 'Port',
+    },
+    'sqlServerSetting_user_label': {
+        'en': 'Username',
+        'de': 'Benutzername',
+    },
+    'sqlServerSetting_password_label': {
+        'en': 'Password',
+        'de': 'Passwort',
+    },
+    'sqlServerSetting_database_label': {
+        'en': 'Databasename',
+        'de': 'Datenbankname',
+    },
+    'sqlServerSetting_check_label': {
+        'en': 'Check Connection',
+        'de': 'Verbindung testen',
+    }
+};
---
+++ main.js
@@ -0,0 +1,174 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v1.23.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+const mysql = require('mysql');
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+/**
+ * The adapter instance
+ * @type {ioBroker.Adapter}
+ */
+let adapter;
+let mySqlPool;
+
+/**
+ * Starts the adapter instance
+ * @param {Partial<ioBroker.AdapterOptions>} [options]
+ */
+function startAdapter(options) {
+    // Create the adapter and define its methods
+    return adapter = utils.adapter(Object.assign({}, options, {
+        name: 'sql-logger',
+
+        // The ready callback is called when databases are connected and adapter received configuration.
+        // start here!
+        ready: main, // Main method defined below for readability
+
+        // is called when adapter shuts down - callback has to be called under any circumstances!
+        unload: (callback) => {
+            try {
+                adapter.log.info('cleaned everything up...');
+                callback();
+            } catch (e) {
+                callback();
+            }
+        },
+
+        // is called if a subscribed object changes
+        objectChange: (id, obj) => {
+            if (obj) {
+                // The object was changed
+                adapter.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+            } else {
+                // The object was deleted
+                adapter.log.info(`object ${id} deleted`);
+            }
+        },
+
+        // is called if a subscribed state changes
+        stateChange: (id, state) => {
+            if (state) {
+                // The state was changed
+                adapter.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+            } else {
+                // The state was deleted
+                adapter.log.info(`state ${id} deleted`);
+            }
+        },
+
+        // Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
+        // requires "common.message" property to be set to true in io-package.json
+        message: (obj) => {
+            processMessage(obj);
+        },
+    }));
+}
+
+function main() {
+    // The adapters config (in the instance object everything under the attribute "native") is accessible via
+    // adapter.config:
+    adapter.log.info('config option1: ' + adapter.config.option1);
+    adapter.log.info('config option2: ' + adapter.config.option2);
+
+    /*
+        For every state in the system there has to be also an object of type state
+        Here a simple template for a boolean variable named "testVariable"
+        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+    */
+    adapter.setObject('testVariable', {
+        type: 'state',
+        common: {
+            name: 'testVariable',
+            type: 'boolean',
+            role: 'indicator',
+            read: true,
+            write: true,
+        },
+        native: {},
+    });
+
+    // in this template all states changes inside the adapters namespace are subscribed
+    adapter.subscribeStates('*');
+
+    /*
+        setState examples
+        you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+    */
+    // the variable testVariable is set to true as command (ack=false)
+    adapter.setState('testVariable', true);
+
+    // same thing, but the value is flagged "ack"
+    // ack should be always set to true if the value is received from or acknowledged from the target system
+    adapter.setState('testVariable', { val: true, ack: true });
+
+    // same thing, but the state is deleted after 30s (getState will return null afterwards)
+    adapter.setState('testVariable', { val: true, ack: true, expire: 30 });
+
+    // examples for the checkPassword/checkGroup functions
+    adapter.checkPassword('admin', 'iobroker', (res) => {
+        adapter.log.info('check user admin pw iobroker: ' + res);
+    });
+
+    adapter.checkGroup('admin', 'admin', (res) => {
+        adapter.log.info('check group user admin group admin: ' + res);
+    });
+}
+
+function processMessage(msg) {
+    adapter.log.info('Message: ' + msg.command);
+
+    if(msg.command === 'checkConnection') {
+        checkConnection(msg);
+    }
+}
+
+function createMySqlPool(config) {
+    adapter.log.debug('Create connection to ' + config.hostname + ' on port ' + config.port + ' with user ' + config.user + ' for db ' + config.database);
+
+    mySqlPool = mysql.createPool({
+        connectionLimit: 5,
+        host: config.hostname,
+        port: config.port,
+        user: config.user,
+        password: config.password,
+        database: config.database
+    });
+}
+
+function checkConnection(msg) {
+    adapter.log.info('Check connection to sql server');
+
+    if(mySqlPool == null) {
+        createMySqlPool(msg.message.config);
+    }
+
+    mySqlPool.getConnection(function(err, connection) {
+        if(err) {
+            adapter.log.error('No connection to database: ' + err.message);
+            adapter.sendTo(msg.from, msg.command, {state: 'false'}, msg.callback);
+            return false;
+        } else {
+            adapter.log.info('SQL Connected');
+            adapter.sendTo(msg.from, msg.command, {state: 'true'}, msg.callback);
+
+            connection.destroy();
+        }
+    });
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export startAdapter in compact mode
+    module.exports = startAdapter;
+} else {
+    // otherwise start the instance directly
+    startAdapter();
+}
